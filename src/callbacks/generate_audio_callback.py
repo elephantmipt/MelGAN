04988994f31157be4808d9786bc18a64a925298f
Nikita <Balaganskij.NN@phystech.edu>
2020-06-27 19:39:02 +0300

    add wandb logging

--- src/callbacks/generate_audio_callback.py
+++ src/callbacks/generate_audio_callback.py
@@ -12,7 +12,7 @@ class GenerateAudioCallback(Callback):
     def __init__(self, epochs: List[int] = None, mel_path: str = None, out_name: str = None):
         super().__init__(order=CallbackOrder.External)
         if epochs is None:
-            epochs = [i+1 for i in range(0, 200, 10)]
+            epochs = [i + 1 for i in range(0, 200, 10)]
         self.epochs = epochs
         self.mel_path = mel_path or "data/LJSpeech-1.1/wavs/LJ001-0006.mel"
         if out_name is None:
@@ -38,5 +38,11 @@ class GenerateAudioCallback(Callback):
             audio = audio.clamp(min=-MAX_WAV_VALUE, max=MAX_WAV_VALUE - 1)
             audio = audio.short()
             audio = audio.cpu().detach().numpy()
+            try:
+                import wandb
+                wandb.log({"examples": [wandb.Audio(audio, caption="Nice", sample_rate=22050)]})
+            except:
+                Warning("Can't import wandb")
+
             out_path = self.out_name + f"_{runner.epoch}.wav"
             write(out_path, 22050, audio)
